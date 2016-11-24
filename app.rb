40f9e9b2b24d06a25ce1225b27fd2028746182b5
George Eaton <george_eaton@hotmail.co.uk>
2016-11-24 10:04:24 +0000

    made it look nicer

--- app.rb
+++ app.rb
@@ -11,14 +11,14 @@ enable :sessions
 
   post '/names' do
     p params
-    session['player1'] = params[:player1]
-    session['player2'] = params[:player2]
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
     redirect to('/play')
   end
 
   get '/play' do
-    @player1 = session['player1']
-    @player2 = session['player2']
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb(:play)
   end
 
