04a78d1f569adf64add8d881128221bfe9b1984b
Pea M. Tyczynska <CrystalPea@github.com>
2016-11-25 14:59:34 +0000

    Added a feature test for unsuccesful attack

--- spec/features/3.attack_confirmation_spec.rb
+++ spec/features/3.attack_confirmation_spec.rb
@@ -1,11 +1,21 @@
 require './app'
 
 RSpec.feature "Attacking player 2", :type => :feature do
-  scenario "Confirm attack" do
+  scenario "Confirm succesful attack" do
     sign_in_and_play
     attack
-    expect(page).to have_content("You have administered a slap round the chops to Pea!")
+    message = "You have administered a slap round the chops to Pea!"
+    expect(page).to have_content(message)
     click_button("Ok, back to battle!")
     expect(page).to have_content("HP")
+  end
+
+  scenario "Confirm unsuccesful attack" do
+    sign_in_and_play
+    allow($game.player2).to receive(:losthp).and_return(false)
+    attack
+    message = "Your attack was unsuccessful, you got lucky Pea"
+    expect(page).to have_content(message)
+
   end
 end
